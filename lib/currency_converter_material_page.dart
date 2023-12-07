9b32ebf6f0a6d773156060e6234a6951c159203e
Lychee80 <Lychee.776@gmail.com>
2023-12-06 22:19:09 -0800

    hi

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -7,9 +7,21 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      backgroundColor:Colors.grey,
         body: Center(
-      child: Text('Hello World'),
-    )
+           child: Column(
+            mainAxisAlignment:MainAxisAlignment.center,
+              children: [
+          Text('0', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold, color:Color.fromARGB(156, 71, 74, 138))),
+        TextField(
+          style: TextStyle(color: Colors.white),
+          decoration: InputDecoration(
+            hintText:'Please enter the amount of INR',
+          )
+        ),
+        ],
+        ),
+        ),
     );
   }
 }
